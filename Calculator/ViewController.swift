8fe6debca1441940ad808c95be9cda98a2cc864f
Vladimir Berezin <92182846+Oldmakerrr@users.noreply.github.com>
2022-10-14 08:31:44 +0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Vladimir Berezin on 14.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
